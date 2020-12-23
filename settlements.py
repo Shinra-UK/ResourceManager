be9ffbc6afd30aafcb0c6516f568657c45431947
Shinra-UK <76250567+Shinra-UK@users.noreply.github.com>
2020-12-23 12:22:14 +0000

    Add create_task

--- settlements.py
+++ settlements.py
@@ -1,4 +1,5 @@
 import utilities
+import tasks
 
 
 class Settlement(utilities.Entity):
@@ -14,8 +15,22 @@ class Settlement(utilities.Entity):
         self.ore = 0
         self.metal = 0
         self.gp = 0
+        self.tasks = []
         self.msg = ""
 
     AMENDABLE = ("food", "wood", "stone", "ore", "metal", "gp")
     EDITABLE = ("leader", "importing", "exporting", "inventory")
     directory = []
+
+    def create_task(self, name, duration, description):
+        name = name.title()
+        existing = utilities.find(self.tasks, "name", name)
+        if existing is None:
+            new = tasks.Task(name, duration, description)
+            self.tasks.append(new)
+            new.msg = f'{new.name} Has been created'
+            print(new.msg)
+            return new
+        else:
+            existing.msg = f'{existing.name} already exists'
+            return existing
