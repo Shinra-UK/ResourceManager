2fca1cb962a13aa7e9d4bdc774b83ef6c4217369
Shinra-UK <76250567+Shinra-UK@users.noreply.github.com>
2020-12-23 08:34:35 +0000

    Add food attribute to Settlement

--- settlements.py
+++ settlements.py
@@ -4,6 +4,7 @@ import utilities
 class Settlement(utilities.Entity):
     def __init__(self, name):
         self.name = name
+        self.food = 0
         self.wood = 0
         self.stone = 0
         self.ore = 0
@@ -11,5 +12,5 @@ class Settlement(utilities.Entity):
         self.gp = 0
         self.msg = ""
 
-    AMENDABLE = ("wood", "stone", "ore", "metal", "gp")
+    AMENDABLE = ("food", "wood", "stone", "ore", "metal", "gp")
     directory = []
